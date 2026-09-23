506d1e953979632fc27de18d232ef08fae6d618c
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-09-23 19:58:15 +0500

    Q2 A1

--- A1(L230828).py
+++ A1(L230828).py
@@ -1,3 +1,5 @@
+# Q1
+
 def pattern_Count(text, pattern):
     count = 0
     pattern_length = len(pattern)
@@ -7,3 +9,27 @@ def pattern_Count(text, pattern):
     return count
 
 print(pattern_Count("ACGTACGTACGTGT", "GT"))
+
+
+#Q2
+
+def frequent_words(text, k):
+    freq_map = {}
+    for i in range(len(text) - k + 1):
+        kmer = text[i:i+k]
+        if kmer in freq_map:
+            freq_map[kmer] += 1
+        else:
+            freq_map[kmer] = 1
+
+
+    max_count = max(freq_map.values())
+    
+    frequent_kmers = []
+    for kmer, count in freq_map.items():
+        if count == max_count:
+            frequent_kmers.append(kmer)
+            
+    return frequent_kmers
+
+print(frequent_words("CGTTTTGAACATTTTCAACAAGTTTTGCAACATTTT ", 4))
