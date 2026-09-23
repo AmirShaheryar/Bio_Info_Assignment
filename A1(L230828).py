1b4b790496dd117f44e5710d8b20ab996407456c
Shaheryar Amir <shaheryaraamir13@gmail.com>
2026-09-23 19:41:30 +0500

    Q1 A1

---
+++ A1(L230828).py
@@ -0,0 +1,9 @@
+def pattern_Count(text, pattern):
+    count = 0
+    pattern_length = len(pattern)
+    for i in range(len(text) - pattern_length + 1):
+        if text.startswith(pattern, i):
+            count += 1
+    return count
+
+print(pattern_Count("ACGTACGTACGT", "CG"))
